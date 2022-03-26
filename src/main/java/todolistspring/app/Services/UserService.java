2a4fadecbaf1b1c6436550f81c7e5f5dc1d66e6a
Jaron van Well <49275554+JaronVW@users.noreply.github.com>
2022-03-26 17:04:26 +0100

    implemented bcrypt password hashing

--- src/main/java/todolistspring/app/Services/UserService.java
+++ src/main/java/todolistspring/app/Services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import todolistspring.app.Models.User;
 import todolistspring.app.Repositories.UserRepository;
@@ -18,11 +19,15 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     public void addNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
